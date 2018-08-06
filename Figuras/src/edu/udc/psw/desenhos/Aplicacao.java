d961ff9058b0dbfb70f334b8702bf40a16b99ea8
GalbPires <37561564+GalbPires@users.noreply.github.com>
2018-08-06 19:20:22 -0300

    Figuras Geometricas - Programa sem alterações

---
+++ Figuras/src/edu/udc/psw/desenhos/Aplicacao.java
@@ -0,0 +1,13 @@
+
+package edu.udc.psw.desenhos;
+
+import edu.udc.psw.gui.JanelaDesenho;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		JanelaDesenho frame = new JanelaDesenho();
+		frame.setVisible(true);
+	}
+
+}
